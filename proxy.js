1561070514c6225e42cabdc318e725a4bbda87bd
takashiAg <crawd4274@gmail.com>
2019-08-28 00:08:33 +0900

    QR test

--- proxy.js
+++ proxy.js
@@ -1,7 +1,10 @@
-var http = require('http');
+var https = require('https');
 var fs = require('fs');
 var url = require('url');
 var httpProxy = require('http-proxy');
+
+var ssl_server_key = 'server_key.pem';
+var ssl_server_crt = 'server_crt.pem';
 
 let wan_port=process.env.WAN || 8080
 let lan_port=process.env.LAN || 3000
@@ -21,6 +24,8 @@ function requestHandler(req, res) {
   proxy.web(req, res, {target: server});
 }
 
-http.createServer(requestHandler).listen(wan_port);
+var options = {pfx: fs.readFileSync('mysslserver.pfx')}
+
+https.createServer(options,requestHandler).listen(wan_port);
 console.log('listening http://0.0.0.0:' + wan_port);
 console.log('proxy to http://localhost:' + wan_port);
