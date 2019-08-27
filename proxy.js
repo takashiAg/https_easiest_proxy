2dc5c6f1eae849081fb1b4a8439b33dd500e62f2
takashiAg <crawd4274@gmail.com>
2019-08-27 23:56:33 +0900

    firstcommit

---
+++ proxy.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var httpProxy = require('http-proxy');
+
+let wan_port=process.env.WAN || 8080
+let lan_port=process.env.LAN || 3000
+
+// var port = process.env.PORT || 8090;
+var logPath = './log';
+var server = 'http://localhost:'+lan_port;
+
+function requestHandler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  var urlobj = url.parse(req.url, true);
+
+  console.log(req.method + ' ' + urlobj.pathname + '\n');
+
+  var proxy = httpProxy.createProxyServer();
+  
+  proxy.web(req, res, {target: server});
+}
+
+http.createServer(requestHandler).listen(wan_port);
+console.log('listening http://0.0.0.0:' + wan_port);
+console.log('proxy to http://localhost:' + wan_port);
